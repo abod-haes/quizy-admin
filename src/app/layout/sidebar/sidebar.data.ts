37a286b3da53411eeed8bc0901cd76fd4836fe89
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-06-19 16:52:50 +0300

    refactor: simplify sidebar

--- src/app/layout/sidebar/sidebar.data.ts
+++ src/app/layout/sidebar/sidebar.data.ts
@@ -1,30 +1,15 @@
-import { Folder } from 'lucide-react'
-
+import { LayoutDashboard } from 'lucide-react'
 
 import type { SidebarItem } from '@/app/layout/sidebar/sidebar.types'
 import { APP_ROUTES } from '@/app/router/route-object.type'
 
 export const primarySidebarItems: SidebarItem[] = [
   {
-    id: 'pages',
-    labelKey: 'sidebar.items.pages',
-    to: APP_ROUTES.pages.path,
-    icon: Folder,
+    id: 'dashboard',
+    labelKey: 'sidebar.items.dashboard',
+    to: APP_ROUTES.dashboard.path,
+    icon: LayoutDashboard,
     end: true,
-    activeMatch: {
-      include: [APP_ROUTES.pages.path + '/add', APP_ROUTES.pages.path + '/view/:id', APP_ROUTES.pages.path + '/edit/:id'],
-    },
-  },
-
-  {
-    id: 'faqs',
-    labelKey: 'sidebar.items.faqs',
-    to: APP_ROUTES.faqs.path,
-    icon: Folder,
-    end: true,
-    activeMatch: {
-      include: [APP_ROUTES.faqs.path + '/add', APP_ROUTES.faqs.path + '/view/:id', APP_ROUTES.faqs.path + '/edit/:id'],
-    },
   },
 ]
 
