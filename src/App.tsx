7685708c4940cf69576c3734efea3c6ba11f39cf
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-08-13 19:58:12 +0300

    fix(ui): load responsive overrides from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 
 import { appRouter } from '@/app/router/app.route'
+import '@/responsive.css'
 
 function App() {
   return <RouterProvider router={appRouter} />
