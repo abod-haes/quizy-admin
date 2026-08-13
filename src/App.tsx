f9b5e60db83168dca14a8e0fc8d0f0b821224a4a
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-08-13 20:47:26 +0300

    style(ui): load shared control system

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 
 import { appRouter } from '@/app/router/app.route'
 import '@/responsive.css'
+import '@/control-system.css'
 
 function App() {
   return <RouterProvider router={appRouter} />
