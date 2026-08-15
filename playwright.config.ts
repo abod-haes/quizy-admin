5aaddb289e071771ce106bf34df6692cae161a93
Jawad Ghareeb <146003194+JawadGhareeb@users.noreply.github.com>
2026-08-15 11:48:55 +0300

    test(e2e): define chromium project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests/e2e',
@@ -13,4 +13,10 @@ export default defineConfig({
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 })
