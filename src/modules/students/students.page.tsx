f0f0cbd2abbaeb53d63ea46ba277040f8c117ccf
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-08-20 11:47:02 +0300

    fix: add password field to student admin form

--- src/modules/students/students.page.tsx
+++ src/modules/students/students.page.tsx
@@ -1,3 +1,5 @@
+import '@/modules/students/student-password-config'
+
 import { StudentsPage as StudentsCrudPage } from '@/modules/content-crud/pages/academic-content-crud.page'
 
 export default function StudentsManagementPage() {
